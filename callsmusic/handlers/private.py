96d4e20910bb4c85aa3a944f65133a47659d7e8a
SAF ONE <77989182+AsmSafone@users.noreply.github.com>
2021-05-14 13:40:15 +0600

    Add files via upload

---
+++ callsmusic/handlers/private.py
@@ -0,0 +1,41 @@
+# Calls Music 2 - Telegram bot for streaming audio in group calls
+# Copyright (C) 2021  Roj Serbest
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+from pyrogram import Client
+from pyrogram.types import InlineKeyboardButton
+from pyrogram.types import InlineKeyboardMarkup
+from pyrogram.types import Message
+
+from ..helpers.filters import other_filters2
+
+
+@Client.on_message(other_filters2)
+async def start(_, message: Message):
+    await message.reply_text(
+        f"""👋🏻 **Hello!**
+
+I Only Works In @AsmSafone😇 
+Request Song At @SafoTheBot !! 😌""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "CHANNEL", url="https://t.me/AsmSafone"
+                    ),
+                    InlineKeyboardButton(
+                        "SUPPORT", url="https://t.me/safothebot"
+                    )
+                ]
+            ]
+        )
+    )
